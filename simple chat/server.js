7d0f86b40d5cf4a1cf773ae50e93fdb25f2dea61
liuding <liuding@ifmuse.com>
2017-03-20 11:55:56 +0800

    add simple chat

--- simple chat/server.js
+++ simple chat/server.js
@@ -2,3 +2,33 @@
 /*
 * Socket
 */
+
+var app 	= require("express")() ;
+var http	= require("http").Server(app) ;
+var io	 	= require("socket.io")(http) ;
+
+app.get("/" , function(req , res){
+	res.status(200);
+	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection" , function(socket){
+	console.log("a client connected!");
+
+	// disconnect
+	socket.on("disconnect" , function(){
+		console.log("the client disconnected!");
+	}) ;
+
+	// chat mess
+	socket.on("chat mess" , function(mes){
+		console.log("chatting mess "+ mes);
+	})
+})
+
+http.listen(3000 , function(){
+	console.log("server is running at port 3000");
+}) ;
+
+
+
